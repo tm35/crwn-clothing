8fc31a5f9b6b80d64c1c155f1cd8d6380667dd5f
tm35 <travis.itf@gmail.com>
2021-11-11 22:00:21 +0200

    buttons

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -49,11 +49,12 @@ class SignIn extends React.Component {
                         label="password"
                         required />
                     
-                    <CustomButton type="submit" value="Submit Form" > Sign in</CustomButton>
-                    <CustomButton value="Submit Form" onClick={signInWithGoogle}> 
-                        {' '}
-                        Sign in with Google{' '}
-                    </CustomButton>
+                    <div className='buttons'>
+                        <CustomButton type="submit" value="Submit Form" > Sign in</CustomButton>
+                        <CustomButton value="Submit Form" onClick={signInWithGoogle} isGoogleSignIn> 
+                            Sign in with Google
+                        </CustomButton>
+                    </div>
                 </form>
             </div>
         )
