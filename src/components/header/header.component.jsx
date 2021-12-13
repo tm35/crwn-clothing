f2040f13a49283b80fd43514f8c9a3d6e695decc
tm35 <travis.itf@gmail.com>
2021-12-14 00:30:57 +0200

    Redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { auth } from "../../firebase/firebase.utils";
+
+import CartIcon from "../cart-icon/cart-icon.component";
+
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
@@ -20,6 +23,7 @@ const Header = ({ currentUser }) => (
                 :
                 <Link className='option' to='/signin'>SIGN IN</Link>
             }
+            <CartIcon />
         </div>
     </div>
 );
